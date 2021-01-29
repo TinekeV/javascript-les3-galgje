99e4ef61bb4394856460c7a7d62a109967cbe717
TinekeV <tinekevlieger@gmail.com>
2021-01-29 13:00:27 +0100

    GamesWon function - fixed and tested

--- gamelogic.js
+++ gamelogic.js
@@ -18,13 +18,46 @@ function displayWordSoFar(word, guesses) {
 }
 
 
-
 function isGameWon(word, guesses) {
-  // WRITE ME
+  const letterArray = word.split('');
+
+  for (let index = 0; index < letterArray.length; index++) {
+    const letterInWord = letterArray[index];
+    const letterGuessed = guesses.includes(letterInWord);
+
+    if (!letterGuessed) {
+      return false;
+    }
+
+    if (letterGuessed) {
+      return true;
+    }
+  }
+
 }
 
 function isGameLost(word, guesses) {
-  // WRITE ME
+  // DOEL FUNCTIE: "isGameLost should return true if you've guessed 7 wrong letters or more"
+  // DOEL FUNCTIE: functie laat true returnen als speler 7 of meer letters verkeerd heeft
+  // DUS if statements
+  // Eentje voor 7 of meer: guesses <= 7
+  // Eentje voor 7 of minder: guesses >= 7
+
+  const letterArray = word.split('');
+
+  for (let index = 0; index < letterArray.length; index++) {
+    const letterInWord = letterArray[index];
+    const letterGuessed = guesses.includes(letterInWord);
+
+    if (letterGuessed.length >= 7) {
+      return false;
+    }
+
+    if (letterGuessed.length <= 7) {
+      return true;
+    }
+  }
+
 }
 
 module.exports = {
