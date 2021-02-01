5d8f83f6658afdae913291f1f2b371d6d1959ab2
TinekeV <tinekevlieger@gmail.com>
2021-02-01 11:33:23 +0100

    gameLost function fixed and tested. full game runs

--- gamelogic.js
+++ gamelogic.js
@@ -17,36 +17,43 @@ function displayWordSoFar(word, guesses) {
     return output
 }
 
-
 function isGameWon(word, guesses) {
-  const letterArray = word.split('');
+  // console.log('WORD:' , word)
+  // console.log('GUESSES:', guesses)
+  const wordInLetterArray = word.split('');
 
-  for (let index = 0; index < letterArray.length; index++) {
-    const letterInWord = letterArray[index];
-    const letterGuessed = guesses.includes(letterInWord);
-
-    if (!letterGuessed) {
-      return false;
-    }
-
-    if (letterGuessed) {
-      return true;
+  for (let index = 0; index < wordInLetterArray.length; index++) {
+    const letterInWord = wordInLetterArray[index];
+    //console.log(letterInWord);
+    const guessedGameWon = guesses.includes(letterInWord);
+    //console.log(guessedGameWon);
+    if (!guessedGameWon) {
+      return false
     }
   }
+
+  return true
 
 }
 
 function isGameLost(word, guesses) {
-  // DOEL FUNCTIE: functie geef TRUE ('VERLOREN) als gebruiker 7 of meer letters verkeerd invult
-  // DUS if statements voor aantal/lengte guesses
-  // Eentje voor 7 of meer: aantal verkeerd geraden letters <= 7
-  // Eentje voor 7 of minder: aantal verkeerd geraden letters >= 7
+  let count = 0
+  for (let i = 0; i < guesses.length; i++) {
+    const guessedLetter = guesses[i];
+    //console.log(guessedLetter);
+    const totalGuesses = word.includes(guessedLetter)
+    //console.log('WAT WORDT ER GELOGD:', totalGuesses);
 
-let wrongGuesses = guesses.length;
-  if (wrongGuesses >= 7) {
+    if (!totalGuesses) {
+      count++;
+      //console.log('LOOPT DE COUNTER?', count)
+    }
+  }
+
+  if (count >= 7) {
     return true
   }
-  if (wrongGuesses <= 7) {
+  if (count < 7) {
     return false
   }
 }
--- index.js
+++ index.js
@@ -3,14 +3,32 @@ const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
 
 function game(word, guesses) {
   console.log("Dit heb je tot nu toe geraden: ", guesses);
+
+  // laat zien wat er al geraden is:
+  console.log(displayWordSoFar(word,guesses));
+
   const letter = question("Raad een letter: ");
 
   // voeg de geraden letter toe aan de array met guesses
   guesses.push(letter);
 
+  // GEWONNEN
+  const winner = isGameWon(word, guesses);
+    if (winner) {
+      console.log('CONGRATS, YOU WON!');
+      return
+  }
+  // VERLOREN
+  const loser = isGameLost(word, guesses);
+    if (loser) {
+      console.log('YOU LOST, SORRY!');
+      return
+    }
+
   // volgende ronde! we roepen game nog een keer aan
   game(word, guesses);
 }
+
 
 console.log(`
 __________  
